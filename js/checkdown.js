87d5456b576f3f4448b0f4f7eea705abae8fbcca
Samuel Mesquita <samuelmesq@gmail.com>
2012-11-27 17:15:08 -0200

    correcting class assignment
    changed manual class names to variable

--- js/checkdown.js
+++ js/checkdown.js
@@ -16,6 +16,7 @@
 	,	document = window.document
 	,	defaults = {
 			defaultValue: '',
+			defaultText : '',
 			onBeforeShow: function(){},
 			onAfterShow: function(){}
 		};
@@ -27,6 +28,13 @@
 		this.label 		= $(this.element).find('span');
 		
 		this.options 	= $.extend({}, defaults, options);
+		this.classes	= {
+			checkdown : 'checkdown',
+			active 	  : 'active',
+			selected  : 'selected',
+			disabled  : 'disabled',
+			multiple  : 'multiple'
+		},
 		this._defaults 	= defaults;
 		this._name 		= pluginName;
 
@@ -39,10 +47,12 @@
 
 		this.options.defaultText = this.label.text();
 
+		$(obj.element).addClass(this.classes.checkdown);
+
 		if( optionsLength > 0 ){
 			for(var i = 0; i < optionsLength; i++) {
 				var opt = $(obj.opts[i]);
-				if( opt.hasClass('selected') ){ obj.updateInputValue() }					
+				if( opt.hasClass(this.classes.selected) ){ obj.updateInputValue() }					
 			}
 		}
 		
@@ -50,30 +60,34 @@
 	};
 
 	Plugin.prototype.initEvents = function(){
-		var obj = this
+		var obj   	= this
 		,	ul 		= $(this.element).find('ul')
 		,	options = this.opts
 
 		// show options
 		$(obj.element).live('click', function(e){
 			var $this = $(this);
-			if( !$this.hasClass('disabled') && !$this.hasClass('active') ){
-				$this.removeClass('active').addClass('active');
+
+			$('.' + obj.classes.checkdown).removeClass(obj.classes.active);
+
+			if( !$this.hasClass(obj.classes.disabled) && !$this.hasClass(obj.classes.active) ){
+				$this.removeClass(obj.classes.active).addClass(obj.classes.active);
 			}
+
 			return false;
 		});
 
 		$(document).click(function(){
-			$(obj.element).removeClass('active');
+			$(obj.element).removeClass(obj.classes.active);
 		});
 
 		// add option
 		options.live('click', function(){
 			var me = $(this);
-			me.toggleClass('selected');
+			me.toggleClass(obj.classes.selected);
 
-			if(!ul.hasClass('multiple'))
-				ul.find('li').not(this).removeClass('selected');
+			if(!ul.hasClass(obj.classes.multiple))
+				ul.find('li').not(this).removeClass(obj.classes.selected);
 
 			obj.updateInputValue();
 		});
@@ -90,7 +104,7 @@
 		};
 
 		for (var i = opts.length - 1; i >= 0; i--) {
-			if ( $(opts[i]).hasClass('selected') ){
+			if ( $(opts[i]).hasClass(obj.classes.selected) ){
 				temps.value.push( $(opts[i]).data('value'));
 				(temps.text.length < 2 ) ? temps.text.push( $(opts[i]).text() ) : null;
 			}
